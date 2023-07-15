1925e4a8bb556d9d4855c96b87c601ccdd305165
simplyarsa <simplyarsa15@gmail.com>
2023-07-15 21:40:01 +0530

    changes

--- controllers/chatbot.js
+++ controllers/chatbot.js
@@ -1,13 +1,15 @@
 import { Configuration, OpenAIApi } from "openai";
+import dotenv from 'dotenv';
+
+dotenv.config();
 
 const configuration = new Configuration({
-  apiKey: "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",
+  apiKey: process.env.OPENAI_API_KEY,
 });
 const openai = new OpenAIApi(configuration);
 
 export const chatbot=async(req,res)=>{
     const {questionBody}=req.body;
-    // res.status(200).json({message: {content: questionBody}})
     const completion = await openai.createChatCompletion({
         model: "gpt-3.5-turbo",
         messages: [{role: "user", content: questionBody}],
